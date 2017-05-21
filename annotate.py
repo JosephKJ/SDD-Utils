1e1c3b173c83e260a10e37d55c7d33b616e2d9c1
Joseph KJ <josephkj20@gmail.com>
2017-05-21 07:17:02 +0530

    Update

--- annotate.py
+++ annotate.py
@@ -1,5 +1,25 @@
-def main():
+def split_and_annotate():
     pass
 
 if __name__ == '__main__':
-    main()
+    videos_to_be_processed = {'bookstore':(0,1,2,3,4,5,6),
+                              'coupa':(0,1,2,3),
+                              'deathCircle':(0,1,2,3,4),
+                              'gates':(0,1,2,3,4,5,6,7,8),
+                              'hyang':(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14),
+                              'little':(0,1,2,3),
+                              'nexus':(0,1,2,3,4,5,6,7,8,9,10,11),
+                              'quad':(0,1,2,3)}
+    videos_to_be_processed = {'bookstore': (0),
+                              'coupa': (),
+                              'deathCircle': (),
+                              'gates': (),
+                              'hyang': (),
+                              'little': (),
+                              'nexus': (),
+                              'quad': ()}
+
+
+    percent_of_test_images = 0.3
+
+    split_and_annotate(videos_to_be_processed, percent_of_test_images)
